a68ae783ca3d74db3db235a3fd753c5f26fa91dd
Harish Pandey <pandeyharish394@gmail.com>
2024-01-06 20:05:19 +0545

    complete toggle button

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+import "./App.css";
+import Toggle from "./Toggle";
+
+const App = () => {
+  return (
+    <div>
+      <Toggle />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "remixicon/fonts/remixicon.css";
+import ThemeProvider from "./context/ThemeProvider.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
